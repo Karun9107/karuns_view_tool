e371511f751dc4fe627f5fe67c14e382ed859692
Karun9107 <karunrajagopal9107@gmail.com>
2021-04-23 01:57:36 +0530

    Custom gem initial commit

---
+++ lib/karuns_view_tool.rb
@@ -0,0 +1,10 @@
+# frozen_string_literal: true
+
+require_relative "karuns_view_tool/version"
+
+require_relative "karuns_view_tool/renderer"
+
+module KarunsViewTool
+  class Error < StandardError; end
+  # Your code goes here...
+end
---
+++ lib/karuns_view_tool/version.rb
@@ -0,0 +1,5 @@
+# frozen_string_literal: true
+
+module KarunsViewTool
+  VERSION = "0.1.0"
+end
